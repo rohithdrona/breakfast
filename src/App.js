73c074f8d58436d3448baf6bfcbbf6b085d13efa
Rohith <srohith30@gmail.com>
2023-11-25 23:11:12 +0530

    items change

--- src/App.js
+++ src/App.js
@@ -5,9 +5,27 @@ const RandomItemSelector = () => {
 
   // Sample list of items with id and item name
   const items = [
-    { id: 1, item: "Item 1" },
-    { id: 2, item: "Item 2" },
-    { id: 3, item: "Item 3" },
+    { id: 1, item: "Neer Dose" },
+    { id: 2, item: "Rave Dose" },
+    { id: 3, item: "Normal Dose" },
+    { id: 4, item: "Idli" },
+    { id: 5, item: "Vegeatable Bath" },
+    { id: 6, item: "Avalakki Pongal" },
+    { id: 7, item: "Chapati" },
+    { id: 8, item: "Parota" },
+    { id: 9, item: "BisileBath" },
+    { id: 10, item: "Chitranna" },
+    { id: 11, item: "Uppittu" },
+    { id: 12, item: "Normal Avalakki" },
+    { id: 13, item: "Huli Avalakki" },
+    { id: 14, item: "Kai Sasuve Avalakki" },
+    { id: 15, item: "Akki Rotti" },
+    { id: 16, item: "Raagi Rotti" },
+    { id: 17, item: "Godi Rotti" },
+    { id: 18, item: "Pundigatti" },
+    { id: 19, item: "Guliyappa" },
+    { id: 20, item: "Shavige" },
+    { id: 21, item: "Ganesha Shavige" },
     // Add more items here as needed
   ];
 
