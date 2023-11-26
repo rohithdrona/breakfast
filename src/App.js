a8614ccdf5976fd565ce02fc7911ee1e58be7573
Rohith <srohith30@gmail.com>
2023-11-26 11:11:44 +0530

    Item changes

--- src/App.js
+++ src/App.js
@@ -2,30 +2,34 @@ import React, { useState } from "react";
 import "./App.css";
 const RandomItemSelector = () => {
   const [selectedItem, setSelectedItem] = useState(null);
-
   // Sample list of items with id and item name
   const items = [
-    { id: 1, item: "Neer Dose" },
-    { id: 2, item: "Rave Dose" },
-    { id: 3, item: "Normal Dose" },
-    { id: 4, item: "Idli" },
-    { id: 5, item: "Vegeatable Bath" },
-    { id: 6, item: "Avalakki Pongal" },
-    { id: 7, item: "Chapati" },
-    { id: 8, item: "Parota" },
-    { id: 9, item: "BisileBath" },
-    { id: 10, item: "Chitranna" },
-    { id: 11, item: "Uppittu" },
-    { id: 12, item: "Normal Avalakki" },
-    { id: 13, item: "Huli Avalakki" },
-    { id: 14, item: "Kai Sasuve Avalakki" },
-    { id: 15, item: "Akki Rotti" },
-    { id: 16, item: "Raagi Rotti" },
-    { id: 17, item: "Godi Rotti" },
-    { id: 18, item: "Pundigatti" },
-    { id: 19, item: "Guliyappa" },
-    { id: 20, item: "Shavige" },
-    { id: 21, item: "Ganesha Shavige" },
+    {
+      id: 1,
+      item: "Dose",
+      variety: ["Normal Dose", "Neer Dose", "Rave Dose", "Ragi Dose"],
+    },
+    { id: 2, item: "Idli", variety: ["Normal Idli", "Rave Idli"] },
+    { id: 3, item: "Vegeatable Bath", variety: [] },
+    { id: 4, item: "Avalakki Pongal", variety: [] },
+    { id: 5, item: "Chapati", variety: [] },
+    { id: 6, item: "Parota", variety: [] },
+    { id: 7, item: "BisileBath", variety: [] },
+    { id: 8, item: "Chitranna", variety: [] },
+    { id: 9, item: "Uppittu", variety: [] },
+    {
+      id: 10,
+      item: "Avalakki",
+      variety: ["Normal Avalakki", "Huli Avalakki", "Kai Sasuve Avalakki"],
+    },
+    {
+      id: 11,
+      item: "Rotti",
+      variety: ["Akki Rotti", "Raagi Rotti", "Godi Rotti"],
+    },
+    { id: 12, item: "Pundigatti", variety: [] },
+    { id: 13, item: "Guliyappa", variety: [] },
+    { id: 14, item: "Shavige", variety: ["Normal Shavige", "Ganesha Shavige"] },
     // Add more items here as needed
   ];
 
@@ -33,19 +37,25 @@ const RandomItemSelector = () => {
     // Get a random index to select a random item from the list
     const randomIndex = Math.floor(Math.random() * items.length);
     const randomItem = items[randomIndex];
+
     setSelectedItem(randomItem);
   };
 
   return (
     <div className="container">
       <button className="centered-button" onClick={handleButtonClick}>
-        Select Random Item
+        Breakfast
       </button>
       {selectedItem && (
-        <div className="centered-data">
-          <h3>Breakfast:</h3>
-          <p>Name : {selectedItem.item}</p>
-        </div>
+        <>
+          <div className="centered-data">
+            <h3>{selectedItem.item}</h3>
+            {selectedItem.variety &&
+              selectedItem.variety.map((eachitem) => {
+                return <div>{eachitem}</div>;
+              })}
+          </div>
+        </>
       )}
     </div>
   );
